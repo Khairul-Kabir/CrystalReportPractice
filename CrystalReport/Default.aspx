3e9bdaaed3d9a8d18b5a6f9526d3ea63cd39f9fb
Khairul Kabir <kabir@syntecglobal.net>
2021-04-15 11:38:17 +0600

    first commit

---
+++ CrystalReport/Default.aspx
@@ -0,0 +1,14 @@
+﻿<%@ Page Title="Home Page" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="CrystalReport._Default" %>
+
+<%@ Register assembly="CrystalDecisions.Web, Version=13.0.4000.0, Culture=neutral, PublicKeyToken=692fbea5521e1304" namespace="CrystalDecisions.Web" tagprefix="CR" %>
+
+<asp:Content ID="BodyContent" ContentPlaceHolderID="MainContent" runat="server">
+
+    <div class="jumbotron">
+        <h1>Crystal Report</h1>
+       
+    </div>
+    <CR:CrystalReportViewer ID="CrystalReportViewer1" runat="server" AutoDataBind="true" />
+    <asp:Button ID="Button1" runat="server" Text="View Report" OnClick="Button1_Click" />
+
+</asp:Content>
---
+++ CrystalReport/Default.aspx.cs
@@ -0,0 +1,44 @@
+﻿using CrystalDecisions.CrystalReports.Engine;
+using CrystalDecisions.Shared;
+using CrystalReport.Model;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace CrystalReport
+{
+    public partial class _Default : Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            SqlConnection con = new SqlConnection("Data Source=localhost\\SQLEXPRESS;Initial Catalog=SPTest;User ID=sa;Password=sa");
+            SqlCommand cmd = new SqlCommand("Select * from Customer",con);
+            SqlDataAdapter sad = new SqlDataAdapter(cmd);
+            DataSet ds = new DataSet();
+            sad.Fill(ds);
+            //SPTestEntities dbContext = new SPTestEntities();
+            //List<Customer> customers = dbContext.Customers.ToList();
+            
+            //customers.Fill
+
+            ReportDocument crp = new ReportDocument();
+            crp.Load(Server.MapPath("CrystalReport1.rpt"));
+            crp.SetDataSource(ds.Tables["table"]);
+            CrystalReportViewer1.ReportSource = crp;
+            crp.ExportToHttpResponse(ExportFormatType.PortableDocFormat, Response, false, "Report");
+
+
+
+        }
+    }
+}
